2eac26187397702491f2ba9fdef741550dc58d91
malcolmfreelancer <malcolmfreelancer@gmail.com>
2015-02-24 17:28:23 -0600

    Update host_setup.sh

--- host_setup.sh
+++ host_setup.sh
@@ -7,7 +7,7 @@ cat ~/.ssh/looplingo_vbox_ssh.pub | ssh ubuntu@192.168.56.2 "mkdir -p ~/.ssh && 
 ## install a local directory to contain our files, setup deployment
 mkdir -p ~/.looplingo/conf
 virtualenv --no-site-packages ~/.looplingo/env
-~/.looplingo/env/bin/pip install Fabric boto
+~/.looplingo/env/bin/pip install Fabric boto tabulate
 
 ## make sure fab is easy to run
 echo "alias fab=\"~/.looplingo/env/bin/python ~/.looplingo/env/bin/fab\"" >> ~/.bashrc
