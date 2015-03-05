25dde38ca8b82e202289ef8febaec52e568cc7eb
malcolmfreelancer <malcolmfreelancer@gmail.com>
2015-03-05 11:41:10 -0600

    fixes to host_setup.sh

--- host_setup.sh
+++ host_setup.sh
@@ -6,6 +6,7 @@ cat ~/.ssh/looplingo_vbox_ssh.pub | ssh ubuntu@192.168.56.2 "mkdir -p ~/.ssh && 
 
 ## install a local directory to contain our files, setup deployment
 mkdir -p ~/.looplingo/conf
+sudo easy_install virtualenv
 virtualenv --no-site-packages ~/.looplingo/env
 ~/.looplingo/env/bin/pip install Fabric boto tabulate
 
@@ -14,5 +15,5 @@ echo "alias fab=\"~/.looplingo/env/bin/python ~/.looplingo/env/bin/fab\"" >> ~/.
 source ~/.bashrc
 curl -o ~/.looplingo/conf/fabric_locals.py "https://raw.githubusercontent.com/LoopLingo/vbox_setup/master/fabric_locals.py"
 
-sudo -u root echo "if [ \"\${BASH-no}\" != \"no\" ]; then [ -r ~/.bashrc ] && source ~/.bashrc; fi" >> /etc/profile
-sudo -u root echo "192.168.56.2 local-dev.lingopoints.com" >> /etc/hosts
+sudo -s "echo \"if [ \\\"\\\${BASH-no}\\\" != \\\"no\\\" ]; then [ -r ~/.bashrc ] && source ~/.bashrc; fi\" >> /etc/profile"
+sudo -s "echo \"192.168.56.2 local-dev.lingopoints.com\" >> /etc/hosts"
