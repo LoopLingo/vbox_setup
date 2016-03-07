54b24383265ba98b164863a374bd11220e38f8dc
malcolmfreelancer <malcolmfreelancer@users.noreply.github.com>
2016-03-07 15:52:31 -0800

    proto in fabfile_locals

--- fabric_locals.py
+++ fabric_locals.py
@@ -1,6 +1,6 @@
 development = {
-    "url_base": "local-dev.lingopoints.com",
-    "proto": "http",
+    "url_base": "https://local-dev.lingopoints.com",
+    "proto": "https",
     "ssh_key": "~/.ssh/looplingo_vbox_ssh",
     "github_ssh_key": "PATH_TO_KEY_HERE"
     }
